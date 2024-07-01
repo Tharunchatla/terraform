5edb490522bd5fe4af5f7d6a537e96c7be390a8e
Tharun chatla <157670457+Tharunchatla@users.noreply.github.com>
2024-07-01 20:06:18 +0530

    Delete module-block-1 directory

--- module-block-1/userdata2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt update -y
-sudo apt install apache2 -y
-sudo systemctl start apache2
-sudo systemctl enable apache2
-echo "<h1>this is server-2 and server running with apache</h1>" | sudo tee /var/www/html/index.html
-sudo systemctl restart apache2
